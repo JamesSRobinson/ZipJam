a4346608caa436f3fbe413869a1b655509472577
James Robinson <j@mesrobinson.co.uk>
2015-09-05 22:50:03 +0100

    update to previous commit

---
+++ updateReleaseStructure.bat
@@ -0,0 +1,3 @@
+copy C:\Users\Jamie\Documents\GitHubVisualStudio\ZipJam\ZipJamGUI\ZipJamGUI\bin\Debug\ZipJamGUI.exe C:\Users\Jamie\Documents\GitHubVisualStudio\ZipJam\ReleaseStructure\ZipJam
+copy C:\Users\Jamie\Documents\GitHubVisualStudio\ZipJam\ZipArchiver\Debug\ZipArchiver.exe C:\Users\Jamie\Documents\GitHubVisualStudio\ZipJam\ReleaseStructure\ZipJam\lib
+copy C:\Users\Jamie\Documents\GitHubVisualStudio\ZipJam\ZipExtractor\Debug\ZipExtractor.exe C:\Users\Jamie\Documents\GitHubVisualStudio\ZipJam\ReleaseStructure\ZipJam\lib
